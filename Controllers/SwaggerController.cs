bf86119c82b83f40c6dd33d8a207c3e4c49f0f71
jason <1092783119@qq.com>
2020-10-14 00:15:51 +0800

    init project

---
+++ Controllers/SwaggerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace swagger.Controllers
+{
+    /// <summary>
+    /// Swagger Controller
+    /// </summary>
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "V1")]
+    public class SwaggerController : ControllerBase
+    {
+        /// <summary>
+        /// get swagger by id.
+        /// </summary>
+        /// <param name="id">swagger id</param>
+        /// <returns></returns>
+        [HttpGet("Get")]
+        public async Task<Swagger> Get(int id)
+        {
+            return await Task.FromResult<Swagger>(new Swagger { Id = id, Name = "hhh" });
+        }
+
+        /// <summary>
+        /// Create Swagger
+        /// </summary>
+        /// <param name="createSwaggerModel"></param>
+        /// <returns></returns>
+        /// <response code="200">Create success</response>
+        [ProducesResponseType(typeof(Swagger), 200)]
+        [ProducesResponseType(typeof(Expression), 500)]
+        [HttpPost("Create")]
+        public async Task<Swagger> Create(CreateSwaggerModel createSwaggerModel)
+        {
+            return await Task.FromResult<Swagger>(new Swagger { Id = 1, Name = createSwaggerModel.Name });
+        }
+
+        /// <summary>
+        /// Create swagger Model
+        /// </summary>
+        public class CreateSwaggerModel
+        {
+            /// <summary>
+            /// New swagger name
+            /// </summary>
+            public string Name { get; set; }
+        }
+
+        /// <summary>
+        /// Swagger entity
+        /// </summary>        
+        public class Swagger
+        {
+            /// <summary>
+            /// swagger id
+            /// </summary>
+            public int Id { get; set; }
+            /// <summary>
+            /// swagger name
+            /// </summary>
+            /// <example>Test</example> 
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace swagger
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger API V1", Version = "v1" });
+                c.SwaggerDoc("v2", new OpenApiInfo { Title = "Swagger API V2", Version = "v2" });
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "Swagger.xml");
+                c.IncludeXmlComments(filePath);
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger API V1");
+                });
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
